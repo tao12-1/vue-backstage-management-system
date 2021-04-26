e997e1cfb67031546f24432a0af26dc8b66f53e9
张桃 <1786582070@qq.com>
2021-04-26 19:30:07 +0800

    完交用户功能列表的开发

--- src/main.js
+++ src/main.js
@@ -5,7 +5,14 @@ import './plugins/element.js'
 import './assets/fonts/iconfont.css'
 import './assets/css/global.css'
 import axios from 'axios'
+// 配置请求根路径
 axios.defaults.baseURL='http://timemeetyou.com:8889/api/private/v1/'
+axios.interceptors.request.use(config=>{
+  console.log(config)
+  config.headers.Authorization=window.sessionStorage.getItem('token')
+  // 在最后必须return config
+  return config
+})
 Vue.prototype.$http=axios
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,45 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import  Login from '../components/Login'
+import Login from '../components/Login'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
+
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path:'/',
-    redirect:'/login'
+    path: '/',
+    redirect: '/login'
   },
   {
-    path:'/login',
-    component:Login
+    path: '/login',
+    component: Login
   },
   {
-    path:'/home',
-    component:Home
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      {
+        path: '/welcome',
+        component: Welcome
+      },
+      {
+        path: '/users',
+        component: Users
+      }
+    ]
   }
 ]
 
 const router = new VueRouter({
   routes
 })
-router.beforeEach((to,from,next)=>{
-  if(to.path==='/login') return next();
-  const tokenStr=window.sessionStorage.getItem('token')
-  if(!tokenStr) return next('/login')
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next();
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
   next()
 })
 export default router
