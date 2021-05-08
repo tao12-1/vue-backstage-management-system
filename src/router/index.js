52b66d988897bd74f46e5348604387ad6f4f8232
张桃 <1786582070@qq.com>
2021-05-08 08:59:39 +0800

    完成了报表功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,8 @@ import Params from '../components/goods/Params.vue'
 import GoodsList from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -63,6 +65,10 @@ const routes = [
      {
        path:'/orders',
        component:Order
+     },
+     {
+       path:'/reports',
+       component:Report
      }
     ]
   },
